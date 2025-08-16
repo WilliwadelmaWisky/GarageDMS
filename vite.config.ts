3638bd100cd9017e76f7f3c1fd0f6a25d11b6748
WilliwadelmaWisky <minecraftprofile52@gmail.com>
2025-08-16 11:45:07 +0300

    Added path shortcuts.

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
       { find: "@components", replacement: "/src/components" },
       { find: "@features", replacement: "/src/features" },
       { find: "@hooks", replacement: "/src/hooks" },
-      { find: "@types", replacement: "/src/types" },
+      { find: "@datatypes", replacement: "/src/types" },
     ],
   },
 })
