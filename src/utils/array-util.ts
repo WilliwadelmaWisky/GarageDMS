158605ae7439fbdf7385f64148dcd7461a5395b9
WilliwadelmaWisky <minecraftprofile52@gmail.com>
2025-09-02 21:05:48 +0300

    Added and modified array helper function.

--- src/utils/array-util.ts
+++ src/utils/array-util.ts
@@ -23,6 +23,45 @@ export function normalizeIndex<T>(index: number, array: Array<T>): number {
 
 /**
  * 
+ * @param index 
+ * @param array 
+ * @returns 
+ */
+export function get<T>(index: number, array: Array<T>): T {
+    const i = normalizeIndex(index, array);
+    return array[i];
+}
+
+/**
+ * 
+ * @param array 
+ * @returns
+ */
+export function last<T>(array: Array<T>): T {
+    return get(-1, array);
+}
+
+
+/**
+ * 
+ * @param startIndex 
+ * @param endIndex 
+ * @param array 
+ * @param predicate 
+ * @returns 
+ */
+export function previousIndex<T>(startIndex: number, array: Array<T>, predicate: (element: T, index: number) => boolean) {
+    for (let i = startIndex; i >= 0; i--) {
+        if (predicate(array[i], i)) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+/**
+ * 
  * @param startIndex 
  * @param endIndex 
  * @param array 
@@ -39,6 +78,11 @@ export function nextIndex<T>(startIndex: number, array: Array<T>, predicate: (el
     return -1;
 }
 
+// ------------------------------------------------------------------------------------------------
+//
+// ------------------------------- STATE CHANGE ARRAY FUNCTIONS -----------------------------------
+//
+// ------------------------------------------------------------------------------------------------
 
 /**
  * 
@@ -55,12 +99,25 @@ export function add<T>(element: T, array: Array<T>): Array<T> {
 
 /**
  * 
- * @param index 
- * @param element
  * @param array 
+ * @param elements 
  * @returns
  */
-export function insert<T>(index: number, element: T, array: Array<T>): Array<T> {
+export function addRange<T>(array: Array<T>, elements: Array<T>): Array<T> {
+    return [
+        ...array,
+        ...elements
+    ];
+}
+
+/**
+ * 
+ * @param array 
+ * @param index 
+ * @param element
+ * @returns
+ */
+export function insert<T>(array: Array<T>, index: number, element: T,): Array<T> {
     if (index < 0 || index > array.length) {
         return array;
     }
@@ -74,12 +131,31 @@ export function insert<T>(index: number, element: T, array: Array<T>): Array<T> 
 
 /**
  * 
- * @param index 
- * @param count 
  * @param array 
+ * @param index 
+ * @param elements 
  * @returns 
  */
-export function del<T>(index: number, count: number, array: Array<T>): Array<T> {
+export function insertRange<T>(array: Array<T>, index: number, elements: Array<T>): Array<T> {
+    if (index < 0 || index > array.length) {
+        return array;
+    }
+
+    return [
+        ...array.slice(0, index),
+        ...elements,
+        ...array.slice(index)
+    ];
+}
+
+/**
+ * 
+ * @param array 
+ * @param index 
+ * @param count 
+ * @returns 
+ */
+export function deleteRange<T>(array: Array<T>, index: number, count: number = 1): Array<T> {
     if (index < 0 || index >= array.length || count <= 0) {
         return array;
     }
@@ -92,12 +168,24 @@ export function del<T>(index: number, count: number, array: Array<T>): Array<T> 
 
 /**
  * 
- * @param index 
- * @param element 
  * @param array 
+ * @param predicate 
  * @returns 
  */
-export function replace<T>(index: number, element: T, array: Array<T>): Array<T> {
+export function deleteBy<T>(array: Array<T>, predicate: (element: T) => boolean): Array<T> {
+    return [
+        ...array.filter(element => !predicate(element))
+    ]
+}
+
+/**
+ * 
+ * @param array 
+ * @param index 
+ * @param element 
+ * @returns 
+ */
+export function replace<T>(array: Array<T>, index: number, element: T): Array<T> {
     if (index < 0 || index >= array.length) {
         return array;
     }
@@ -112,37 +200,60 @@ export function replace<T>(index: number, element: T, array: Array<T>): Array<T>
 /**
  * 
  * @param index 
+ * @param element 
  * @param array 
- * @returns
+ * @returns 
  */
-export function moveup<T>(index: number, array: Array<T>): Array<T> {
-    if (index <= 0 || index >= array.length) {
-        return array;
-    }
-
-    return [
-        ...array.slice(0, index - 1),
-        array[index],
-        array[index - 1],
-        ...array.slice(index + 1)
-    ];
-}
-
-/**
- * 
- * @param index 
- * @param array 
- * @returns
- */
-export function movedown<T>(index: number, array: Array<T>): Array<T> {
-    if (index < 0 || index >= array.length - 1) {
+export function replaceRange<T>(array: Array<T>, index: number, elements: Array<T>): Array<T> {
+    if (index < 0 || index >= array.length) {
         return array;
     }
 
     return [
         ...array.slice(0, index),
-        array[index + 1],
-        array[index],
-        ...array.slice(index + 2)
+        ...elements,
+        ...array.slice(index + elements.length)
+    ];
+}
+
+/**
+ * 
+ * @param array 
+ * @param index 
+ * @param count 
+ * @param by
+ * @returns
+ */
+export function moveup<T>(array: Array<T>, index: number, count: number = 1, by: number = 1): Array<T> {
+    if (index <= 0 || index >= array.length || count <= 0 || by <= 0) {
+        return array;
+    }
+
+    return [
+        ...array.slice(0, index - by),
+        ...array.slice(index, index + count),
+        ...array.slice(index - by, index),
+        ...array.slice(index + count)
+    ];
+}
+
+/**
+ * 
+ * @param array 
+ * @param index 
+ * @param count
+ * @param by 
+ * @returns
+ */
+export function movedown<T>(array: Array<T>, index: number, count: number = 1, by: number = 1): Array<T> {
+    if (index < 0 || index >= array.length - 1 || count <= 0 || by <= 0) {
+        return array;
+    }
+
+    return [
+        ...array.slice(0, index),
+        ...array.slice(index + count, index + count + by),
+        ...array.slice(index, index + count),
+        ...array.slice(index + count + by)
     ];
 }
